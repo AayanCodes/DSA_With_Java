759056c7e518680083759aa1ec6a061847d947f2
Mohd Aayan <aayanmalik3114@gmail.com>
2026-07-21 23:38:12 +0530

    Implement reverse array functionality in reverseArray
    Fix syntax errors and print the reversed array.

---
+++ Arrays/reverseArray.java
@@ -0,0 +1,17 @@
+public class reverseArray{
+  public static void main(String[] args) {
+    
+    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    
+    int n = arr.length;
+    
+    for (int i = n-1; i >=0; i--) {
+    
+      system.out.print(arr[i] + " ");  // print the reverse array in same order!
+
+    }
+
+  }
+
+}
+      
