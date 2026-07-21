fecc2ab98b2b26224f90dfb287df3683b8f031fa
Mohd Aayan <aayanmalik3114@gmail.com>
2026-07-21 23:12:12 +0530

    Add SumOfArray class to calculate array sum

---
+++ Arrays/SumOfArray.java
@@ -0,0 +1,15 @@
+public class SumOfArray {
+  public static void main(String[] args) {
+    int[] arr = {15, 40, 7, 89, 23};
+
+    int sum = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    
+    }
+    System.out.println("Sum of Array Elements = " + sum);
+  
+  }
+
+}
