205e3a1532cdcde92a84eba44c159d2d8799dfe0
Mohd Aayan <aayanmalik3114@gmail.com>
2026-07-22 22:46:58 +0530

    Add FindSecondSmallestElement class to find second smallest

---
+++ Arrays/FindSecondSmallestElement.java
@@ -0,0 +1,22 @@
+public class FindSecondSmallest {
+
+    public static void main(String[] args) {
+
+        int[] arr = {15, 40, 7, 89, 23};
+
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] < smallest) {
+                secondSmallest = smallest;
+                smallest = arr[i];
+            } else if (arr[i] < secondSmallest && arr[i] != smallest) {
+                secondSmallest = arr[i];
+            }
+        }
+
+        System.out.println("Second Smallest Element = " + secondSmallest);
+    }
+}
