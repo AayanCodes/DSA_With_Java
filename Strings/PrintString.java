ce0f72e9ce579cf5513f6795d1b9de05043e2c1e
Mohd Aayan <aayanmalik3114@gmail.com>
2026-08-01 21:02:09 +0530

    Add PrintString class to display student details
    This class prints the name and branch of a student.

---
+++ Strings/PrintString.java
@@ -0,0 +1,23 @@
+public class PrintString {
+    public static void main(String[] args) {
+
+        String name = "Mohd Aayan";
+
+        System.out.println(name);
+
+        String branch = "Computer Science and Engineering";
+
+        System.out.println(branch);
+
+    }
+
+}    
+
+
+/* OUTPUT :
+
+
+Mohd Aayan
+Computer Science and Engineering
+
+  */
