e9280a0a1f42d01cc684478e9ead588efa00b3f9
Mohd Aayan <aayanmalik3114@gmail.com>
2026-08-02 23:08:33 +0530

    Create Upper_LowerCase class for string case conversion
    Added a class to demonstrate string case conversion.

---
+++ Strings/Upper_Case.java
@@ -0,0 +1,20 @@
+public class Upper_LowerCase {
+    public static void main(String[] args) {
+
+        String str = "mohd aayan";
+
+        System.out.println("Upper Case = " + str.toUpperCase());
+
+        System.out.println("Lower Case = " + str.toLowerCase());
+        
+    }
+}
+
+
+/*  OUTPUT:
+
+Upper Case MOHD AAYAN
+
+Lower Case mohd aayan
+
+  */
