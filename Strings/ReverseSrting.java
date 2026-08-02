56e6cde290eb86d6c21ed5c0cb7fa353404e28e1
Mohd Aayan <aayanmalik3114@gmail.com>
2026-08-02 23:05:58 +0530

    Add ReverseString class to reverse a string
    Implement a Java program to reverse a given string.

---
+++ Strings/ReverseSrting.java
@@ -0,0 +1,24 @@
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String str = "nayaa dhom";
+
+        String reverse = " ";
+
+        for (int i = str.length() -1; i >= 0; i--) {
+
+            reverse = reverse + str.charAt(i);
+
+        }
+        System.out.println("Reverse String = " + reverse);
+
+    }
+}
+
+
+/*   
+OUTPUT :
+
+mohd aayan
+
+  */
