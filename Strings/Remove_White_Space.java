964342702abc46c3105e903935836bb25d92b7ca
Mohd Aayan <aayanmalik3114@gmail.com>
2026-08-05 23:42:43 +0800

    Add Remove_White_Space class to eliminate spaces

---
+++ Strings/Remove_White_Space.java
@@ -0,0 +1,16 @@
+public class Remove_White_Space {
+
+    public static void main(String[] args) {
+
+        String str = "aayan malik";
+
+        str = str.replace(" " , "");
+
+        System.out.println("String after removing white space = " + str);
+
+
+
+
+    }
+
+}
