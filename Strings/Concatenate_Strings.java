5a81769f8bae6908efb2249d54a6105875a0a8e6
Mohd Aayan <aayanmalik3114@gmail.com>
2026-08-08 17:32:12 +0530

    Add string concatenation example in Java
    Implemented a Java program to concatenate two strings and print the result.

---
+++ Strings/Concatenate_Strings.java
@@ -0,0 +1,19 @@
+public class Concatenate_Strings {
+
+    public static void main(String[] args) {
+
+        String str1 = "Mohd";
+        String str2 = "Aayan";
+
+        String result = str1 + " " + str2;
+
+        System.out.println("Concatenated String = " + result);
+    }
+}
+
+
+/*  OUTPUT :
+
+Concatenate String = Mohd Aayan
+
+  */
