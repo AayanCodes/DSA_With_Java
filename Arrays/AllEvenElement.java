45af7ab0744dbc785972e319e6d651dcad3bc0a7
Mohd Aayan <aayanmalik3114@gmail.com>
2026-08-21 23:54:28 +0530

    Implement AllEvenElements class to print even numbers
    Fix syntax errors and improve formatting.

---
+++ Arrays/AllEvenElement.java
@@ -0,0 +1,11 @@
+public class AllEvenElements {
+  public Static void main(String[] args) {
+    int[] arr = {10, 15, 20, 25, 30, 35, 40};
+    System.out.println("Even Element : ");
+    for (int num : arr) {
+      if (num % 2 == 0;) {
+        System.out.println(num + " ");
+      }
+    }
+  }
+}
