835000b559eab852e1d95fd71ff15665c76198ad
Mohd Aayan <aayanmalik3114@gmail.com>
2026-09-16 22:24:18 +0530

    Add MatrixPrint class to display 2D array elements
    Implement a MatrixPrint class that prints elements of a 2D array in a specific order.

---
+++ Arrays/MatrixPrint.java
@@ -0,0 +1,22 @@
+public class MatrixPrint {
+  public static void main(String[] args ) {
+    int [][] array = new int [][] {{0, 1, 2, 3}, {10, 20, 30, 40}, {50, 60, 70, 80} };
+
+  for (int i = 0; i <array.length; i++) {
+      if(i%2 ==0) {
+        System.out.println(array[i][j] + " ");
+      }
+  } else {
+    for(int j <array[0].length - 1; j >= 0; j++){
+      System.out.println(array[i][j] + " ");
+    }
+  }
+  }
+  }
+}
+
+//OUTPUT :
+// 0 1 2 3 44 33 22 12 21 34 56 44 
+    
+      
+      
